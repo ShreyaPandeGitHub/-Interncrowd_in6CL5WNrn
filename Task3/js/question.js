77f975db50334dba0087f1778365cde4db17d4f9
shreya4401 <109268887+ShreyaPandeGitHub@users.noreply.github.com>
2023-02-22 10:42:47 +0530

    Add files via upload

---
+++ Task3/js/question.js
@@ -0,0 +1,31 @@
+
+const quiz = [
+    {
+        q:' What does CSS stands for?',
+        options:['Cascading Super Sheets','Cascading Style Sheep','Cascading Style Sheets','None of the above'],
+        answer:2
+    },
+    {
+        q:'The full form of PHP is',
+        options:['None of the above','Personal Home Processor','Pretext Hypertext Preprocessor','Hypertext Preprocessor'],
+        answer:3
+
+    },
+    {
+        q:'What is the correct Javascript syntax to write "Hello World" ?',
+        options:['System.out.println("Hello World")','printf("Hello World")','document.write("Hello World")','response.write("Hello World")'],
+        answer:2
+    },
+    {
+        q:'CSS is used for',
+        options:['All of the above','Design a Web Page','Create interactive web pages','Create a Web Page'],
+        answer:1
+        
+    },
+    {
+        q:'To create HTML Page, you need-----',
+        options:['Web Browser','Text Editor','Both a & b','None of these'],
+        answer:2
+    },
+
+]
